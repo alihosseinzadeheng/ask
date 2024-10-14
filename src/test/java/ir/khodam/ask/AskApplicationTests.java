57585b852ba02a6519659a3deb79b0bb97fa0ab4
Ali Hosseinzadeh <hoseinzadeh@jibit.ir>
2024-10-14 14:56:57 +0330

    Starten wir

---
+++ src/test/java/ir/khodam/ask/AskApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.khodam.ask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
